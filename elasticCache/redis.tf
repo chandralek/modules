5b28012c70c518a5b38c89a0092a1dbe0010cf9a
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-24 05:20:18 +0530

    updating modules terraform repo

--- elasticCache/redis.tf
+++ elasticCache/redis.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "redis-cluster" {
   node_type            = var.NODE_TYPE
   num_cache_nodes      = var.NUMBER_OF_NODES
   parameter_group_name = aws_elasticache_parameter_group.redis-5-0-6-pg.name
-  engine_version       = "3.2.10"
+  engine_version       = "5.0.6"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis-subnet-group.name
   security_group_ids   = [aws_security_group.allow_redis_access.id]
