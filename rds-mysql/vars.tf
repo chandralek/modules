4c767073e159f60c2a9f14c752de288added8961
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-28 22:02:53 +0530

    updating modules terraform repo

---
+++ rds-mysql/vars.tf
@@ -0,0 +1,10 @@
+variable "DBUSER" {}
+variable "DBPASS" {}
+variable "VPC_ID" {}
+variable "PRIVATE_SUBNETS" {}
+variable "PUBLIC_SUBNETS" {}
+variable "MGMT_VPC_ID" {}
+variable "TAGS" {}
+variable "INSTANCE_TYPE" {}
+variable "PROD" {}
+variable "NONPROD" {}
