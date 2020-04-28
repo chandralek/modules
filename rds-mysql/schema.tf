28100555a601c4234cd4e8e9c8e1003bcff7487f
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-28 22:51:16 +0530

    updating modules terraform repo

--- rds-mysql/schema.tf
+++ rds-mysql/schema.tf
@@ -5,8 +5,8 @@ resource "null_resource" "mysql-schema-load" {
 git clone https://${var.GITLAB_USER}:${var.GITLAB_PASSWORD}@gitlab.com/batch46/robo-shop/mysql.git
 cd mysql
 gunzip shipping.sql.gz
-mysql -h ${aws_db_instance.mysql-instance.address} -u ${var.DBUSER} -p${var.DBPASS} <shipping.sql
-mysql -h ${aws_db_instance.mysql-instance.address} -u ${var.DBUSER} -p${var.DBPASS} <ratings.sql
+mysql -h ${element(aws_db_instance.mysql-instance.*.address,count.index)} -u ${var.DBUSER} -p${var.DBPASS} <shipping.sql
+mysql -h ${element(aws_db_instance.mysql-instance.*.address,count.index)} -u ${var.DBUSER} -p${var.DBPASS} <ratings.sql
 EOF
   }
 }
