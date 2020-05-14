71dfc75ac6c4c7a9effedafe9763991970032f1d
Chandralekha.s <chandralekha.s@sophos.com>
2020-05-14 19:37:26 +0530

    Updating modules module

--- mongodb/route53.tf
+++ mongodb/route53.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "mongo" {
   zone_id = var.HOSTED_ZONE_ID
-  name    = var.HOSTED_ZONE_NAME
+  name    = local.DNS_NAME
   type    = "A"
-  ttl     = "300"
-  records = ["10.0.0.1"]
+  ttl     = "5"
+  records = [module.ec2.PRIVATE_IP]
 }
