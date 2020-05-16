0411729a578a77fe676f6103550f258b50b259e9
Chandralekha.s <chandralekha.s@sophos.com>
2020-05-16 14:00:41 +0530

    Updating modules module

--- rabbitmq/locals.tf
+++ rabbitmq/locals.tf
@@ -1,4 +1,4 @@
 locals {
   DNS_NAME = "rabbitmq-${var.TAGS["PROJECT"]}-${var.TAGS["ENV"]}.${var.HOSTED_ZONE_NAME}}"
-  NAME_TAG = "rabbitmq-${var.TAGS["ENV"]}-service"
+  NAME_TAG = "rabbitmq-${var.TAGS["PROJECT"]}-${var.TAGS["ENV"]}"
 }
